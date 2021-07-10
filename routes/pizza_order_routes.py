ea7fe63cf503f6fd0dada49847efd7f7b2c4c833
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-07-09 18:10:18 -0600

    init files

---
+++ routes/pizza_order_routes.py
@@ -0,0 +1,42 @@
+from flask import render_template, request, redirect, session
+
+
+class PizzaOrderRoutes:
+    @staticmethod
+    def configure_routes(app):
+        @app.route("/size", methods=["GET", "POST"])
+        def size():
+            if request.method == "GET":
+                return render_template("size.html")
+            elif request.method == "POST":
+                selectedSize = request.form["size"]
+                session["size"] = selectedSize
+                return redirect("flavor")
+
+        @app.route("/flavor", methods=["GET", "POST"])
+        def flavor():
+            if request.method == "GET":
+                return render_template("flavor.html")
+            elif request.method == "POST":
+                selectedFlavor = request.form["flavor"]
+                session["flavor"] = selectedFlavor
+                return redirect("complement")
+
+        @app.route("/complement", methods=["GET", "POST"])
+        def complement():
+            if request.method == "GET":
+                return render_template("complement.html")
+            elif request.method == "POST":
+                selectedComplement = request.form["complement"]
+                session["complement"] = selectedComplement
+                return redirect("extra")
+
+        @app.route("/extra", methods=["GET", "POST"])
+        def extra():
+            if request.method == "GET":
+                return render_template("extra.html")
+            elif request.method == "POST":
+                extraString = request.form.getlist("extra")
+                print(extraString)
+                session["extra"] = extraString
+                return f"extra posted"
